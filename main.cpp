258495d078324aa8acce4fea9a85e070983c3787
Nozarash1 <k0toffnickol@yandex.ru>
2025-02-25 12:56:29 +0200

    25.02.25

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+
+
+    a.setStyleSheet("QMainWindow { background-color: #2E2E2E; }"
+                    "QPushButton { background-color: #4CAF50; color: white; }"
+                    "QLabel { color: white; }");
+
     MainWindow w;
     w.setWindowTitle("Жесть какой текстовый редактор");
-    w.resize(400, 400);
+    w.setWindowState(Qt::WindowMaximized);
+
 
     w.show();
     return a.exec();
